17c0498c49ffb029c30c2ca31988fc6b0856580b
Viktor <vi.h.seo@gmail.com>
2022-01-20 11:40:46 +0100

    Test changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,19 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const config = {
   host: "localhost",
   dialect: "postgres",
-  port: 5432
-}
+  port: 5432,
+};
 
-const sequelize = new Sequelize("localydb", "postgres", "codecloudnine", config);
+const sequelize = new Sequelize(
+  "localydb",
+  "postgres",
+  "codecloudnine",
+  config
+);
 
+console.log(process.env.DB_PASSWORD);
 
 module.exports = sequelize;
