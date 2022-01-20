c5aa855d61caadb45df6ba18d4be38cb2738b72f
Viktor <vi.h.seo@gmail.com>
2022-01-20 13:05:28 +0100

    Checked the files

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,9 +3,10 @@ const User = require("../models/users.model");
 const authMiddleware = async (req, res, next) => {
   try {
     const { uid } = req.session;
+    //TODO change to findbyID
     const user = await User.findOne({ where: { id: uid } });
     if (!user) throw new Error();
-    req.user = user.dataValues;
+    req.user = user;
     next();
   } catch (error) {
     console.log(error);
