5a1f3080b451ce4f277e23f39a334d8676347f66
Viktor <vi.h.seo@gmail.com>
2022-01-21 17:14:05 +0100

    testing

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,11 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const config = {
   host: "localhost",
   dialect: "postgres",
-  port: 5432
-}
+  port: 5432,
+};
 
-const sequelize = new Sequelize("localydb", "postgres", "codecloudnine", config);
-
+const sequelize = new Sequelize("localydb", "postgres", "0000", config);
 
 module.exports = sequelize;
